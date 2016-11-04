0ddca899ad418a0180aa55011c9bd9fcbfd6dd34
Matthew Peck <matt@operable.io>
2016-11-04 16:32:07 -0400

    update echo tests

--- test/commands/echo_test.exs
+++ test/commands/echo_test.exs
@@ -2,17 +2,15 @@ defmodule Cog.Test.Commands.EchoTest do
   use Cog.CommandCase, command_module: Cog.Commands.Echo
 
   test "Repeats whatever it is passed" do
-    response = new_req(args: ["this", "is", "nifty"])
-               |> send_req()
-               |> unwrap()
+    {:ok, response} = new_req(args: ["this", "is", "nifty"])
+    |> send_req()
 
     assert(response == "this is nifty")
   end
 
   test "serializes json when it's passed" do
-    response = new_req(args: [%{"bar" => "baz"}])
-               |> send_req()
-               |> unwrap()
+    {:ok, response} = new_req(args: [%{"bar" => "baz"}])
+    |> send_req()
 
     assert(Poison.decode!(response) == %{"bar" => "baz"})
   end
