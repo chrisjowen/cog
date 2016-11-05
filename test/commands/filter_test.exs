ebb1704f9bb6b2cc88a93edb85cc47c779bea198
Matthew Peck <matt@operable.io>
2016-11-05 16:01:00 -0400

    filter test updates

--- test/commands/filter_test.exs
+++ test/commands/filter_test.exs
@@ -1,49 +1,37 @@
 defmodule Cog.Test.Commands.FilterTest do
   use Cog.CommandCase, command_module: Cog.Commands.Filter
 
-  test "returns an item that matches at a specific key" do
+  test "can match on objects on a specific path" do
     data = %{"foo" => %{"bar" => "stuff", "baz" => "other stuff"}}
 
-    match = new_req(cog_env: data,
-                    options: %{"path" => "foo.bar",
-                               "matches" => "stuff"})
-            |> send_req()
-            |> unwrap()
+    {:ok, match} = new_req(cog_env: data,
+                           options: %{"path" => "foo.bar",
+                                      "matches" => "stuff"})
+    |> send_req()
+
+    {:ok, no_match} = new_req(cog_env: data,
+                           options: %{"path" => "foo.bar",
+                                      "matches" => "other stuff"})
+    |> send_req()
 
     assert(%{foo: %{bar: "stuff",
                     baz: "other stuff"}} = match)
-  end
-
-  test "returns nothing when an item does not match at a specific key " do
-    data = %{"foo" => %{"bar" => "stuff", "baz" => "other stuff"}}
-
-    no_match = new_req(cog_env: data,
-                    options: %{"path" => "foo.bar",
-                               "matches" => "no match here"})
-               |> send_req()
-               |> unwrap()
 
     assert(nil == no_match)
   end
 
-  test "returns an item based on the presence of a key" do
+  test "filters a list of things based on a key" do
     data = %{"foo" => %{"bar" => "stuff"}}
 
-    match = new_req(cog_env: data,
-                    options: %{"path" => "foo.bar"})
-            |> send_req()
-            |> unwrap()
+    {:ok, match} = new_req(cog_env: data,
+                           options: %{"path" => "foo.bar"})
+    |> send_req()
+
+    {:ok, no_match} = new_req(cog_env: data,
+                           options: %{"path" => "foo.baz"})
+    |> send_req()
 
     assert(%{foo: %{bar: "stuff"}} = match)
-  end
-
-  test "returns nothing when the requested key is not present" do
-    data = %{"foo" => %{"bar" => "stuff"}}
-
-    no_match = new_req(cog_env: data,
-                    options: %{"path" => "foo.baz"})
-               |> send_req()
-               |> unwrap()
 
     assert(nil == no_match)
   end
